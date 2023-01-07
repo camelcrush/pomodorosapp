6af521a6c5805265dc94173225dd71a9d02f27a6
camelcrush <dkxm0927@naver.com>
2023-01-07 21:25:05 +0900

    User Interface

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,73 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.play_circle_outline_outlined,
+                  )),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color:
+                                  Theme.of(context).textTheme.headline1!.color),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                              fontSize: 58,
+                              fontWeight: FontWeight.w600,
+                              color:
+                                  Theme.of(context).textTheme.headline1!.color),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
